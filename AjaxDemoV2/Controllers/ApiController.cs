c799aac37b61b84fa9829d03088a67042eff0c96
EricLiao <ericxiang851019@gmail.com>
2024-01-29 20:45:53 +0800

    加入專案檔案。

---
+++ AjaxDemoV2/Controllers/ApiController.cs
@@ -0,0 +1,24 @@
+﻿using AjaxDemoV2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxDemoV2.Controllers
+{
+    public class ApiController : Controller
+    {
+        private MyDBContext _dbContext;
+        public ApiController(MyDBContext dBContext) 
+        {
+            _dbContext = dBContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+            var data = _dbContext.Addresses.Select(x => x.City).Distinct();
+            return Json(data);
+        }
+    }
+}
